465799935b02bc27d1fe986e0ce60bdd2f448e13
aruzhanbur <b_aruzhan@kbtu.kz>
2024-01-23 13:22:15 +0600

    booleans

---
+++ lab2/booleans.py
@@ -0,0 +1,20 @@
+#task1
+print(10 > 9)
+#output: True
+
+#task2
+print(10 == 9)
+#output: False
+
+#task3
+print(10 < 9)
+#output: False
+
+#task4
+print(bool("abc"))
+#output: True
+
+#task5
+print(bool(0))
+#output: False
+
