17c99911c7e8c8affa22a6998108ea218341342f
aruzhanbur <b_aruzhan@kbtu.kz>
2024-01-30 13:46:23 +0600

    inherit

---
+++ lab3/inherit.py
@@ -0,0 +1,16 @@
+#task1
+class Student(Person)
+    
+#task2
+class Person:
+  def __init__(self, fname):
+    self.firstname = fname
+
+  def printname(self):
+    print(self.firstname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike")
+x.printname()
