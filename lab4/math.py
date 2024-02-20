fe0ee3cf41c323434b5b9d0150766cfb822fcb6c
aruzhanbur <b_aruzhan@kbtu.kz>
2024-02-20 13:23:20 +0600

    math

--- lab4/math.py
+++ lab4/math.py
@@ -7,3 +7,27 @@ def torad(x):
 x = int(input())
 z = torad(x)
 print(z)
+
+#task2
+def area(h, a, b):
+    return ((a + b) * h) / 2
+
+h, a, b = int(input()), int(input()), int(input())
+z = area(h, a, b)
+print(z)
+
+#task3
+def area(n, l):
+    return ( n * pow(l, 2) ) / (4 * math.tan(math.pi / n))
+    
+n, l = int(input()), int(input())
+z = area(n, l)
+print(math.floor(z))
+
+#task4
+def area(b, h):
+    return b * h
+    
+b, h = int(input()), int(input())
+z = area(b, h)
+print(z)
