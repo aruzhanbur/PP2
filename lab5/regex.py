47165bbe7193bc1015b29a6165fbcf3c3fc60b19
aruzhanbur <b_aruzhan@kbtu.kz>
2024-02-27 13:55:14 +0600

    redone

--- lab5/regex.py
+++ lab5/regex.py
@@ -1,40 +1,69 @@
 import re
 
+with open("row.txt", "r") as file:
+    lines = file.readlines()
+
 #task1
 p = re.compile("^a[b]*")
-m = p.search("abbbb")
-print(m.group())
+print("Output for task1: ")
+for line in lines:
+    for word in line.split():
+        if p.match(word):
+            print(word)
+print()
+
 
 #task2
-p = re.compile("\w*[a]{1}[b]{2,3}")
-m = p.search("zzzzabbbbbbb")
-print(m.group())
+p2 = re.compile("a[b]{2,3}")
+print("Output for task2: ")
+for line in lines:
+    for word in line.split():
+        if p2.match(word):
+            print(word)
+print()
 
 #task3
-p = re.compile("[a-z]*[_][a-z]*")
-m = p.search("aboba_aboba")
-print(m.group())
+p3 = re.compile("[a-z]*[_][a-z]*")
+print("Output for task3: ")
+for line in lines:
+    for word in line.split():
+        if p3.match(word):
+            print(word)
+print()
 
 #task4
-p = re.compile("[A-Z]{1}[a-z]*")
-m = p.search("Banananaaaaaa")
-print(m.group())
+p4 = re.compile("[A-Z][a-z]+")
+print("Output for task4: ")
+for line in lines:
+    for word in line.split():
+        if p4.match(word):
+            print(word)
+print()
 
 #task5
-p = re.compile("\w[a]{1}\w*b$")
-m = p.search("Banananaaaaaab")
-print(m.group())
+p5 = re.compile("\w[a]\w*b$")
+print("Output for task5: ")
+for line in lines:
+    for word in line.split():
+        if p5.match(word):
+            print(word)
+print()
 
 #task6
-x = "Hello. My name is Aruzhan."
-y = re.sub('[ ,.]', ':', x)
-print(y)
+new = []
+print("Output for task6: ")
+for line in lines:
+    dodo = re.sub(r'[ ,.]', ':', line)
+    new.append(dodo)
+
+r = ''.join(new)
+print(r)
 
 #task7
 def snamle(x):
    x = re.sub(r'_([a-zA-Z])', lambda match: match.group(1).upper(), x)
    return x
-
+print("Output for task7: ")
 y = "Doner_sperchikom"
 result = snamle(y)
 print(result)
